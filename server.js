01d80c17bf870aead5cb388a6f293b1344152ad0
emsumit <72928644+emsumit@users.noreply.github.com>
2024-08-02 12:15:07 +0530

    updated cors origin

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://pocket-notes-react-test.vercel.app/",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   })
 );
