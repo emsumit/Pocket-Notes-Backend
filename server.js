70f8dfe6502975b7772c47bd5f6e4d240ef27eb4
emsumit <72928644+emsumit@users.noreply.github.com>
2024-08-02 12:12:11 +0530

    updates

--- server.js
+++ server.js
@@ -13,13 +13,9 @@ app.use(express.json());
 app.use(
   cors({
     origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   })
 );
-
-app.get("/test-cors", (req, res) => {
-  res.json({ message: "CORS is working!" });
-});
 
 dot.config().parsed;
 
