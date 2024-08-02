6aa233bbe3e5e6412d615e8740a485c3a540c06f
emsumit <72928644+emsumit@users.noreply.github.com>
2024-08-02 12:02:36 +0530

    updates

--- server.js
+++ server.js
@@ -8,7 +8,18 @@ const PORT = 3000;
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
+app.get("/test-cors", (req, res) => {
+  res.json({ message: "CORS is working!" });
+});
 
 dot.config().parsed;
 
